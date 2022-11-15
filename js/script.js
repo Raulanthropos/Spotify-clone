ac2a66a799788aa0be29fb83235ebabc229d40e0
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-11-15 18:21:44 +0200

    updated the async function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 const inputField = document.querySelector(".form-control");
 const searchBtn = document.getElementById("search-btn");
 
-window.onload = async () => {
+window.onload = () => {
     const options = {
       method: "GET"
     };
   
     const baseUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
-    let artistId = "411";
-    const response = await fetch(baseUrl + artistId, options);
-      const artist = await response.json();
-  
-    const artistName = artist.name;
-    const artistPicture = artist.picture_xl;
-    const tracklist = artist.tracklist;
-    console.log(artistName, artistPicture, tracklist);
 
-const searchResult = () => {
-    artistId = inputField.value;
-    console.log(artistId);
+const searchResult = async () => {
+    let artistId = inputField.value;
+    const response = await fetch(baseUrl + artistId, options);
+    const artist = await response.json();
+  const artistName = artist.name;
+  const artistPicture = artist.picture_xl;
+  const tracklist = artist.tracklist;
+  console.log(artistName, artistPicture, tracklist);
+
+    // console.log(artist);
 }
 
     searchBtn.addEventListener('click', searchResult);
