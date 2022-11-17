0d426faa1466b410ebbe0eb3d6216b0b8fae8c80
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-11-17 22:05:49 +0200

    form redirects to the welcome page

--- js/login.js
+++ js/login.js
@@ -9,8 +9,13 @@ const pass1 = "123456";
 const pass2 = "789012";
 const pass3 = "ermahgerd";
 const submitBtn = document.getElementById("log-in");
+const params = new URLSearchParams(window.location.search);
+const usernameValueDos = username.value.trim();
+const userId = params.get(`userNameValueDos`);
+console.log(userId, usernameValueDos);
 
-const validateInputs = () => {
+
+const validateInputs = (e) => {
     const usernameValue = username.value.trim();
     const passwordValue = password.value.trim();
 
@@ -42,76 +47,3 @@ const validateInputs = () => {
 submitBtn.addEventListener('click', validateInputs);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// form.addEventListener('submit', (e) => {
-    // if ((userName.value === user1) || (userName.value === user2) || (userName.value === user3)) {
-    //     switch (userName) {
-    //         case user1: password.value === pass1 ? alert("redirecting you to the home page...") : alert("Wrong username or password! Please try again.")
-    //         break;
-    //         case user2: password.value === pass2 ? alert("redirecting you to the home page...") : alert("Wrong username or password! Please try again.")
-    //         break;
-    //         case user3: password.value === pass3 ? alert("redirecting you to the home page...") : alert("Wrong username or password! Please try again.")
-    //         break;
-    //         default:     console.log(`Sorry, we are out of.`);
-    //     }
-    //     console.log(userName.value, password.value);
-    // } else {
-    //     console.log(userName.value);
-    //     alert("Your Username is not in our database! Would you like to sign up for Spotify?")
-    // }
-//     console.log(userName.value);
-// })
-
-
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//   let messages = []
-//   if (userName.value === '' || userName.value == null) {
-//     messages.push('Name is required')
-//     console.log(userName, messages);
-//   }
-
-//   if (password.value.length <= 6) {
-//     messages.push('Password must be longer than 6 characters')
-//   }
-
-//   if (password.value.length >= 20) {
-//     messages.push('Password must be less than 20 characters')
-//   }
-
-//   if (password.value === 'password') {
-//     messages.push('Password cannot be password')
-//   }
-
-//   if (messages.length > 0) {
-//     e.preventDefault()
-//     errorElement.innerText = messages.join(', ')
-//     console.log(errorElement);
-//   }
-// })
