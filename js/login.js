33c93d00ed1f453a8e521dbb3db17062d5ddf2fe
Raulopoulos <62166476+Raulopoulos@users.noreply.github.com>
2022-11-18 13:26:45 +0200

    changes in login

--- js/login.js
+++ js/login.js
@@ -10,33 +10,63 @@ const pass2 = "789012";
 const pass3 = "ermahgerd";
 const submitBtn = document.getElementById("log-in");
 
-const validateInputs = () => {
+const validateInputs = (e) => {
     const usernameValue = username.value.trim();
     const passwordValue = password.value.trim();
 
-    // if (usernameValue === '' || passwordValue === '') {
-    //     alert("It seems that you did not fill the fields correctly. Please try again.");
-    // } else if (usernameValue === user1 && passwordValue === pass1) {
-    //     alert(`Welcome home, ${user1}! We're redirecting you to the home page...`)
-    // } else if (usernameValue === user2 && passwordValue === pass2) {
-    //     alert(`Welcome home, ${user2}! We're redirecting you to the home page...`)
-    // } else if (usernameValue === user3 && passwordValue === pass3) {
-    //     alert(`Welcome home, ${user3}! We're redirecting you to the home page...`)
-    // } else {
-    //     alert("Either your username or your password, is wrong. Please try again.")
-    // }
+    if (usernameValue === '' || passwordValue === '') {
+        alert("It seems that you did not fill the fields correctly. Please try again.");
+        form.innerHTML =`<form action="login.html" method="get">
+        <div class="login">
+        <input type="text" autocomplete="username" placeholder="Email address or username" id="username">
+        <input type="password" minlength="1" autocomplete="current-password" placeholder="Password" id="password">
+    </div>
+    <div class="getIn">
+        <div class="remember-user">
+            <input id='checkbox' type="checkbox">
+            <label for="checkbox">Remember me</label>
 
-    switch(usernameValue) {
-        case '': alert("It seems that you did not fill the fields. Please try again.");
-        break;
-        case user1: passwordValue === pass1 ? alert(`Welcome home, ${user1}! We're redirecting you to the home page...`) : alert(`Your password is wrong, ${user1}. Please try again`);
-        break;
-        case user2: passwordValue === pass2 ? alert(`Welcome home, ${user2}! We're redirecting you to the home page...`) : alert(`Your password is wrong, ${user2}. Please try again`);
-        break;
-        case user3: passwordValue === pass3 ? alert(`Welcome home, ${user3}! We're redirecting you to the home page...`) : alert(`Your password is wrong, ${user3}. Please try again`);
-        break;
-        default: alert(`Your username is wrong. Please try again`)
+        </div>
+        <button class="btn btn-success mt-1" type="submit" id="log-in">
+            <a href="">LOG IN</a>
+        </button>
+    </div>`
+    } else if (usernameValue === user1 && passwordValue === pass1) {
+        alert(`Welcome home, ${user1}! We're redirecting you to the home page...`)
+    } else if (usernameValue === user2 && passwordValue === pass2) {
+        alert(`Welcome home, ${user2}! We're redirecting you to the home page...`)
+    } else if (usernameValue === user3 && passwordValue === pass3) {
+        alert(`Welcome home, ${user3}! We're redirecting you to the home page...`)
+    } else {
+        alert("Either your username or your password, is wrong. Please try again.")
+        form.innerHTML =`<form action="login.html" method="get">
+        <div class="login">
+        <input type="text" autocomplete="username" placeholder="Email address or username" id="username">
+        <input type="password" minlength="1" autocomplete="current-password" placeholder="Password" id="password">
+    </div>
+    <div class="getIn">
+        <div class="remember-user">
+            <input id='checkbox' type="checkbox">
+            <label for="checkbox">Remember me</label>
+
+        </div>
+        <button class="btn btn-success mt-1" type="submit" id="log-in">
+            <a href="">LOG IN</a>
+        </button>
+    </div>`
     }
+
+    // switch(usernameValue) {
+    //     case '': alert("It seems that you did not fill the fields. Please try again.");
+    //     break;
+    //     case user1: passwordValue === pass1 ? alert(`Welcome home, ${user1}! We're redirecting you to the home page...`) : alert(`Your password is wrong, ${user1}. Please try again`);
+    //     break;
+    //     case user2: passwordValue === pass2 ? alert(`Welcome home, ${user2}! We're redirecting you to the home page...`) : alert(`Your password is wrong, ${user2}. Please try again`);
+    //     break;
+    //     case user3: passwordValue === pass3 ? alert(`Welcome home, ${user3}! We're redirecting you to the home page...`) : alert(`Your password is wrong, ${user3}. Please try again`);
+    //     break;
+    //     default: alert(`Your username is wrong. Please try again`)
+    // }
 };
 
 submitBtn.addEventListener('click', validateInputs);
