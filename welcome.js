61a1f6951d0e6390841deda62b162ef96cf4e386
Damian Olszewski <damianolszewski25@gmail.com>
2022-11-18 09:30:58 +0100

    more cardsssss

--- welcome.js
+++ welcome.js
@@ -61,7 +61,7 @@ let secondRow = document.querySelector("#secondAlbumRow");
 // Card row with albums to redirect to album page.
 const fillPageAlbums = function (listOfSearch) {
   cardstart.innerHTML = `<h1 class="col-12 mb-4">Good Morning</h1>`;
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 12; i++) {
     console.log(listOfSearch.data[i].album.id);
     cardstart.innerHTML += `<div class="col-6 col-md-6 col-lg-6 col-xl-2">
                               <a href="album.html?albumId=${listOfSearch.data[i].album.id}">
